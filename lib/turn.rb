7d2fc6b06714d88434ba5a244e8ba65c200fdb86
April Adams <illusionlace@gmail.com>
2020-08-07 00:25:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ end
 def position_taken?(board, index)
   if (board[index])== "X" || (board[index]) == "O" ||
     true
-  elsif board[index] == " "|| board[index]) == "" || board[index] == nil
+  elsif board[index] == " "|| board[index] == "" || board[index] == nil
     false
   end
 end
