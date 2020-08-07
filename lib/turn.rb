515af91b270828e7a493a9d5fdea1f630e4dad5b
April Adams <illusionlace@gmail.com>
2020-08-07 00:16:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,7 +18,7 @@ def position_taken?(board, index)
     return true
   end
 def valid_move?(board, index)
-  if input_to_index.between?(0,8) && !position_taken?(board,index)
+  if index.between?(0,8) && !position_taken?(board,index)
     return true
   else
     return false
