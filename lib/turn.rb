4a0a0a5029bcdf209ada8178e483e56779cda12f
April Adams <illusionlace@gmail.com>
2020-08-07 00:27:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,22 +10,3 @@ def input_to_index(user_input)
   index = user_input.to_i - 1
   return index
 end
-def position_taken?(board, index)
-  if (board[index])== "X" || (board[index]) == "O" ||
-    true
-  elsif board[index] == " "|| board[index] == "" || board[index] == nil
-    false
-  end
-end
-def valid_move?(board, index)
-  if index > 0
-    index-1
-  end
-  if index > 8
-    false
-  elsif position_taken?(board, index)
-    false
-  else
-    true
-  end
-end
