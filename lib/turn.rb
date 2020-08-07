25605d23e1877f8429e153e6c03a9d1824f487aa
April Adams <illusionlace@gmail.com>
2020-08-07 00:42:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,6 +25,10 @@ def move(board, index, player = "X")
 end
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.chomp
+  user_input = gets.strip
   index = input_to_index(user_input)
+  if valid_move?(board, index) move(board, index, player)
+    turn(board)
+end
+display_board(board)
 end
